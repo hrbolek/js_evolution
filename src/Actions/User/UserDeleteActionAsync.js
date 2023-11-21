5c0997d74848076746f185f6603d451bd563ef2e
Hrbolek <alexandr.stefek@unob.cz>
2023-11-21 09:50:50 +0100

    step 10

--- src/Actions/User/UserDeleteActionAsync.js
+++ src/Actions/User/UserDeleteActionAsync.js
@@ -1,4 +1,20 @@
 import { actions } from "../../Store"
+import { buildPayload, authorizedFetch } from "../authorizedFetch"
+
+const BuildQueryUserDelete = (user) => {
+    return buildPayload({
+        query: `{
+            result: userPage {
+              __typename
+              id
+              name
+              surname
+              email
+            }
+          }`
+    })
+}
+
 export const UserDeleteActionAsync = (user) => (dispatch, getState) => {
     
     dispatch(actions.DeleteItem(user))
